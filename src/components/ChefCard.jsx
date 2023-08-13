b27d5cfd32bdf5ea9c5406091bcf637ac3a65306
Subhro <amerpoth@gmail.com>
2023-08-13 14:52:13 +0530

    url test  7

--- src/components/ChefCard.jsx
+++ src/components/ChefCard.jsx
@@ -1,12 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import "../css/ChefCard.css";
 import { AppContext } from "../context/AppContext";
 
 function ChefCard({orderno,item,accept,loadOrderData,setLoadOrderData}) {
+  let eid = localStorage.getItem("id");
 
-
-
-  const{emp}=useContext(AppContext);
+  const{emp,setEmp}=useContext(AppContext);
   const orderAccept=()=>{
     let url=process.env.REACT_APP_URL+"order/update/maker/"+orderno+"/"+emp.empid+"/3";
     fetch(url,{method:"PUT"})
@@ -23,6 +22,14 @@ function ChefCard({orderno,item,accept,loadOrderData,setLoadOrderData}) {
     
 
   }
+
+  useEffect(()=>{
+    let url=process.env.REACT_APP_URL;
+    fetch(url+"employee/get/"+eid)
+    .then(res=>res.json())
+    .then(res=>setEmp(res))
+    .catch(err=>console.log("error while fetching employee data"))
+  });
   return (
     <div className='d-flex justify-content-between flex-wrap chef-ui-card my-3 mx-3'>
         <label>Order No: <span className='orderNumber'>{orderno}</span></label>
